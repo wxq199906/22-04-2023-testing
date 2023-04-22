7792db8c85a020d86583340669e287408083bc72
wxq199906 <18877899564@163.com>
2023-04-22 14:56:34 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+/* script.js */
+
+function changeColor() {
+    var h1 = document.querySelector('h1');
+    if (h1.style.color === 'blue') {
+      h1.style.color = 'pink';
+    } else {
+      h1.style.color = 'blue';
+    }
+  }
+  
